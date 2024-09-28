6b203de389a5549142875fbaa90a48aaf1967f32
Fabri-404 <135560802+Fabri-404@users.noreply.github.com>
2024-09-28 19:00:35 -0400

    Cambios en header 2

--- componentes/Header.php
+++ componentes/Header.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/componentes/CSS/app.css">
+    <link rel="stylesheet" href="./CSS/app.css">
 </head>
 <body>
     <header class="header">
         <h1>Ticket <span class="highlight"> X </span>  Press</h1>
         <h2>Tu puerta de entrada a los mejores eventos</h2>
+        <div class="white-line"></div>
     </header>
+    
 </body>
 </html>
--- index.php
+++ index.php
@@ -4,12 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/componentes/CSS/app.css">
-<!-- GOOGLE FONTS-->
-    <link href="https://fonts.googleapis.com/css2?family=Angkor&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Playwrite+CO:wght@100..400&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Creepster&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./componentes/CSS/app.css">
 </head>
 <body>
     <?php include 'componentes/Navbar.php'; ?>

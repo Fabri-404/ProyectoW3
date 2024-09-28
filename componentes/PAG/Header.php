ca3aa65ef946726fb5e7ab28c1abae44c21c86a0
Fabri-404 <135560802+Fabri-404@users.noreply.github.com>
2024-09-28 12:48:11 -0400

    first commit

---
+++ componentes/PAG/Header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="path/to/your/styles.css">
+</head>
+<body>
+    <header class="header">
+        <h1>Ticket  <span class="highlight"> X </span> Press</h1>
+        <h2>Tu puerta de entrada a los mejores eventos</h2>
+    </header>
+</body>
+</html>
